65c2d1af3004f7bd774c8862da0032ef753ad06a
Super <38338700+superpowers04@users.noreply.github.com>
2024-04-19 03:55:23 -0400

    Use seperate cache table, fix bugs, meow

--- loose scripts/conf.lua
+++ loose scripts/conf.lua
@@ -1,22 +1,41 @@
 -- Config to table 
 --  Returns a table that you can easily load and save config stuff using normal access like conf.key = "meow"
---  Also caches config stuff so you don't have to constantly use config:load
+--  Also caches config stuff so you don't have to constantly load config stuff. Also also allows config to be used as a normal table on other clients
 -- Made by superpowers04
 
-local conf = {} -- Empty table
-local confLoad = function(this,key) -- The _index function
-    local val = rawget(this,key); 
-    if(val ~= nil) then -- If the value is set, return it
-        return val
-    end 
-    val = config:load(key) -- Otherwise, try to load it
-    if val ~= nil then rawset(this,key,val) end -- Only set table key if something was returned
-    return val
+
+local config = config -- Import main config api to prevent overwriting it from breaking and to speed up access time
+
+
+local configCache = {} -- The actual table holding all of the stuff
+local confLoad, confSave
+if(host:isHost()) then -- Only use actual config library on host, otherwise reference the cache
+	confLoad = function(_,key,...) -- The __index function
+		local val = configCache[key]; 
+		if(val ~= nil) then -- If the value is set, return it
+			-- print('CACHE',key,val) -- Debug prints
+			return val
+		end
+		val = config:load(key) -- Otherwise, try to load it
+		configCache[key] = val -- Set table key
+		-- print('LOAD',key,val) -- Debug prints
+		return val
+	end
+	confSave = function(_,key,value,...)
+		configCache[key] = value -- Sets the table's value
+		config:save(key,value) -- Saves it
+		-- print('SAVE',key,value) -- Debug prints
+	end
+else
+	confLoad = function(_,key,...) -- The __index function
+		return rawget(configCache,key); -- Grab from cache
+	end
+	confSave = function(_,key,value,...)
+		rawset(configCache,key,value) -- Set on cache
+	end
 end
-local confSave = function(this,key,value) 
-  rawset(this,key,value) -- Sets the table's value
-  config:save(key,value) -- Saves it
-end
-local confMT = {__index=confLoad,__newindex=confsave} -- The metatable
+
+local conf = {__cache=configCache,load=confLoad,save=confSave} -- Table with base functions for compatibility
+local confMT = {__index=confLoad,__newindex=confSave} -- The metatable
 setmetatable(conf,confMT) -- set conf to use the metatable
 return conf
